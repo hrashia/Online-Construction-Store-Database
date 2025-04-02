f3eed063fd45205fc15d7eff53498ce4d40609df
Rashia <hdrash77@gmail.com>
2025-04-01 22:21:01 -0500

    Update Top5Customers.sql

--- Top5Customers.sql
+++ Top5Customers.sql
@@ -4,3 +4,11 @@ join orders o on c.CustomerID = o.OrderID
 join products p on o.ProductID = p.ProductID
 order by p.Price DESC 
 limit 5
+
+------------------------------------------------
+
+Jeremy Taylor	967.44
+Thomas Williams	923.1
+Michael Reyes	923.1
+Keith Davis	923.1
+Robert Hammond	906.0
